4173c3cd7be84237b045faba1d0a0f94352bac32
ArtyomSamsonchik <colpachek@gmail.com>
2023-12-05 18:38:23 +0300

    feat: add stories to Checkbox
    Add a description and parameters to the storybook for the new ability to add custom icons to the Checkbox component

--- src/app/ui/checkbox/checkbox.stories.tsx
+++ src/app/ui/checkbox/checkbox.stories.tsx
@@ -1,14 +1,30 @@
+import { ReactElement, createElement } from 'react'
+
+import { CheckedIcon } from '@/icons/checked-icon'
+import { EyeIcon } from '@/icons/eye-icon'
+import { EyeOffIcon } from '@/icons/eye-off-icon'
+import { RadioCheckedIcon } from '@/icons/radio-checked-icon'
+import { RadioUncheckedIcon } from '@/icons/radio-unchecked-icon'
+import { UncheckedIcon } from '@/icons/unchecked-icon'
 import { Meta, StoryObj } from '@storybook/react'
 
 import { Checkbox, CheckboxClasses } from './checkbox'
 
 const checkboxClasses: CheckboxClasses = {
-  icon: 'string',
   indicator: 'string',
   label: 'string',
   root: 'string',
+  unchecked: 'string',
   wrapper: 'string',
 }
+
+const options = ['default', 'eye icon', 'radio icon']
+const customIcons = {
+  checked: [CheckedIcon, EyeIcon, RadioCheckedIcon],
+  unchecked: [UncheckedIcon, EyeOffIcon, RadioUncheckedIcon],
+}
+const getIcons = (state: 'checked' | 'unchecked'): Record<string, ReactElement> =>
+  Object.fromEntries(options.map((o, i) => [o, createElement(customIcons[state][i])]))
 
 const meta = {
   argTypes: {
@@ -20,11 +36,19 @@ const meta = {
           Must be used in conjunction with onCheckedChange.`,
     },
 
-    // TODO: refactor classes prop to use getClassNames helper
+    checkedIcon: {
+      description: `An optional react element that will replace the default icon in the checked state.
+        It is expected that the svg element will be utilized.`,
+      mapping: getIcons('checked'),
+      options,
+      table: { defaultValue: { summary: null }, type: { summary: 'ReactElement' } },
+    },
+
     classes: {
       description: `An object containing the names of the classes corresponding to the
       component slots. Provided classnames wil be merged with default slots classnames.`,
       table: {
+        defaultValue: { summary: 'undefined' },
         type: {
           detail: JSON.stringify(checkboxClasses, null, 2),
           summary: 'CheckboxClasses',
@@ -33,7 +57,6 @@ const meta = {
     },
 
     defaultChecked: {
-      control: false,
       description: `The checked state of the checkbox when it is initially rendered.
         Use when you do not need to control its checked state.`,
       table: { type: { summary: 'boolean' } },
@@ -41,10 +64,17 @@ const meta = {
 
     disabled: { table: { type: { summary: 'boolean' } } },
 
+    icon: {
+      description: `An optional react element that will replace the default icon in an unchecked state.
+        It is expected that the svg element will be utilized.`,
+      mapping: getIcons('unchecked'),
+      options,
+      table: { defaultValue: { summary: null }, type: { summary: 'ReactElement' } },
+    },
+
     name: { table: { type: { summary: 'string' } } },
 
     onCheckedChange: {
-      action: 'onCheckedChange',
       description: `Event handler called when the checked state of the checkbox changes.`,
       table: { type: { summary: '(checked: boolean) => void' } },
     },
@@ -65,13 +95,13 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
+export const Primary: Story = {
   args: { defaultChecked: true, disabled: false },
 }
 
 export const WithLabel = {
   args: {
-    ...Default.args,
+    ...Primary.args,
     label: 'Lorem ipsum dolor.',
   },
 }
